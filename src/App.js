a27b4c48d4171f66e6c4a99709fdc71831c0cdc6
ferdousz <80195995+ferdousz@users.noreply.github.com>
2021-09-26 22:13:31 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Hotels from './components/Hotels/Hotels';
+
+
+
+function App() {
+  return (
+    <div className= "background-img">
+ 
+     <Header/>
+    <Hotels/>
+    </div>
+
+  );
+}
+
+export default App;
