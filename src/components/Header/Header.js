f3492ffec4ac4d7d989b76aa75f5ce7a417ef273
ferdousz <80195995+ferdousz@users.noreply.github.com>
2021-09-26 22:08:59 +0600

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+import './header.css'
+
+const Header = () => {
+   
+    return (
+      <div className="header">
+            <h1>Best <span className = "Header-logo">H</span>otel</h1>
+        <h3>Choose From The 15 Best Hotels in USA </h3>
+
+       
+        </div>
+    
+    );
+};
+
+export default Header;
